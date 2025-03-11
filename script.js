484b652314d3d2429e09c473b8fcda2dc6030395
Taichi320 <akt.0320.taichi.2001@gmail.com>
2025-03-11 16:16:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.body.addEventListener("click", function() {
   window.location.href = "next.html";
 });
+
+function toggleMenu() {
+    document.querySelector(".menu").classList.toggle("open");
+}
