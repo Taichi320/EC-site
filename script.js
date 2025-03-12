66116eb6a53c0b0cd022905d8832cc09d046b6a1
Taichi320 <akt.0320.taichi.2001@gmail.com>
2025-03-12 22:52:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menuIcon");
-    const menu = document.getElementById("menu");
-
-    menuIcon.addEventListener("click", function () {
-        menu.classList.toggle("show");
-    });
-
-    document.addEventListener("click", function (event) {
-        if (!menu.contains(event.target) && event.target !== menuIcon) {
-            menu.classList.remove("show");
-        }
-    });
-});
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
